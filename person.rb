4b685d80cdd9e2dd7754c9e49724b3f941629fbd
AnicetFantomas <94958024+AnicetFantomas@users.noreply.github.com>
2022-06-22 10:31:59 +0200

    Add person, student and teacher classes

---
+++ person.rb
@@ -0,0 +1,26 @@
+class Person 
+    attr_reader :id, :name, :age
+    attr_writer :name, :age
+
+    def initialize(name = "unknown", age, parent_permission = true)
+        @id = Random.rand(1..1000)
+        @name = name
+        @age = age
+        @parent_permission = parent_permission
+    end
+
+    private
+    
+    def is_of_age 
+        if @age >= 18
+            true
+        else 
+            false 
+        end
+    end
+
+    def can_use_services? 
+        if is_of_age && @parent_permission
+            true
+    end
+end 
