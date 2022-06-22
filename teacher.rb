7722921e9f44973f606e05d61555df58c2287d26
AnicetFantomas <94958024+AnicetFantomas@users.noreply.github.com>
2022-06-22 10:49:08 +0200

    Add  teacher  classe

--- teacher.rb
+++ teacher.rb
@@ -1,6 +1,10 @@
 require "./person.rb"
 class Teacher < Person
     def initialize (spetialization)
-        
+        @spetialization = spetialization
+    end
+
+    def can_use_services?
+        true
     end
 end
