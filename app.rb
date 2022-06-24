7ac288e55d7c021449e9e826733dac3105cbc2c6
AnicetFantomas <94958024+AnicetFantomas@users.noreply.github.com>
2022-06-24 16:53:43 +0200

    Add app.rb file

---
+++ app.rb
@@ -0,0 +1,47 @@
+require './person'
+require './book'
+require './student'
+require './teacher'
+require './rental'
+
+class App 
+    def initialize
+        @my_books = []
+        @my_rentals = []
+        @people = []
+      end
+
+      def show_menu
+        puts "Welcome to School Library App!\n\n"
+        puts 'Please choose an option by entering a number:'
+        puts '1 - List all books'
+        puts '2 - List all people'
+        puts '3 - Create a person'
+        puts '4 - Create a book'
+        puts '5 - Create a rental'
+        puts '6 - List all rentals for a given person id'
+        puts "7 - Exit\n\n"
+        gets.chomp
+      end
+
+      def run
+        choice = show_menu
+        case choice
+        when '1'
+          action_list_books
+        when '2'
+          action_list_people
+        when '3'
+          create_person
+        when '4'
+          create_book
+        when '5'
+          create_rental
+        when '6'
+          list_rental
+        else
+          puts 'Thanks You, Bye Bye...'
+          exit
+        end
+    end      
+end
