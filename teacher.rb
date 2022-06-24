f8dad1d1b0b75d5b9dbc43bdd88d31796d0f9328
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-06-24 20:28:24 +0200

    fix rubocop errors

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,5 @@
 require './person'
-<<<<<<< HEAD
-=======
 
->>>>>>> feature2
 class Teacher < Person
   def initialize(age, spetialization, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission)
