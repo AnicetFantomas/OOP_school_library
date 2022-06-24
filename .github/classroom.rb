6a4ab0c0497ba848fb5017aa1ae01a5de46614f0
AnicetFantomas <94958024+AnicetFantomas@users.noreply.github.com>
2022-06-24 12:32:12 +0200

    Add classRoom class

---
+++ .github/classroom.rb
@@ -0,0 +1,15 @@
+class ClassRoom
+    attr_accessor :label
+    attr_reader :students
+  
+    def initialize(label)
+      @label = label
+      @students = []
+    end
+  
+    def add_students(student)
+      @students.push(student)
+      student.classroom = self
+    end
+  end
+  
